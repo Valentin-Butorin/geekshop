c5bb3de818ee822a4153f9281407f74cff3c289a
Valentin Butorin <arch1_cs@mail.ru>
2021-10-09 23:31:27 +0800

    lesson-2

--- products/views.py
+++ products/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from json import load, decoder
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    current_date = datetime.now()
+    context = {
+        'title': 'Geekshop',
+        'current_date': current_date
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    products_list = parse_json('products/fixtures/products.json')
+    context = {
+        'title': 'Geekshop - Каталог',
+        'products': products_list,
+        'card_template': 'products/card_template.html'
+    }
+    return render(request, 'products/products.html', context)
+
+
+def parse_json(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            return load(f)
+    except FileNotFoundError:
+        return []
+    except decoder.JSONDecodeError:
+        return []
