d79e7d50e4cbab907dc754cd53e25c5262ed3e86
Valentin Butorin <arch1_cs@mail.ru>
2021-10-20 22:23:34 +0800

    create users app

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from users.views import login, logout, registration, profile
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
+]
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def login(request):
+    pass
+
+
+def registration(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def profile(request):
+    pass
