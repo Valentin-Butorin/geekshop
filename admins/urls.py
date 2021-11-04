5015b7fa7b731374fea36f9f0b3c81bef7f89469
Valentin Butorin <arch1_cs@mail.ru>
2021-11-04 13:23:04 +0800

    added UserListView

--- admins/urls.py
+++ admins/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from admins.views import index, admin_users_create, admin_users, admin_users_update, admin_users_delete, \
+from admins.views import index, admin_users_create, UserListView, admin_users_update, admin_users_delete, \
     admin_categories, admin_categories_create, admin_categories_update, admin_categories_delete
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
+    path('users/', UserListView.as_view(), name='admin_users'),
     path('users_create/', admin_users_create, name='admin_users_create'),
     path('users_update/<int:id>/', admin_users_update, name='admin_users_update'),
     path('users_delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
