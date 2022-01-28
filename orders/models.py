9d325e23e53ffebb55c919cf71bb049b34fc955e
well <arch1_cs@mail.ru>
2022-01-29 00:52:53 +0800

    lesson-7

--- orders/models.py
+++ orders/models.py
@@ -2,50 +2,62 @@ from django.conf import settings
 from django.db import models
 
 from products.models import Product
+from django.utils.functional import cached_property
 
 
 class Order(models.Model):
-    STATUS_FORMING = 'FM'
-    STATUS_SEND_TO_PROCEED = 'STP'
-    STATUS_PROCEEDED = 'PRD'
-    STATUS_PAID = 'PD'
-    STATUS_CANCEL = 'CNL'
-    STATUS_DONE = 'DN'
+	STATUS_FORMING = 'FM'
+	STATUS_SEND_TO_PROCEED = 'STP'
+	STATUS_PROCEEDED = 'PRD'
+	STATUS_PAID = 'PD'
+	STATUS_CANCEL = 'CNL'
+	STATUS_DONE = 'DN'
 
-    STATUSES = {
-        (STATUS_FORMING, 'формируется'),
-        (STATUS_SEND_TO_PROCEED, 'отправлено в обработку'),
-        (STATUS_PROCEEDED, 'обработано'),
-        (STATUS_PAID, 'оплачено'),
-        (STATUS_CANCEL, 'отменено'),
-        (STATUS_DONE, 'завершено'),
-    }
+	STATUSES = {
+		(STATUS_FORMING, 'формируется'),
+		(STATUS_SEND_TO_PROCEED, 'отправлено в обработку'),
+		(STATUS_PROCEEDED, 'обработано'),
+		(STATUS_PAID, 'оплачено'),
+		(STATUS_CANCEL, 'отменено'),
+		(STATUS_DONE, 'завершено'),
+	}
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    status = models.CharField(choices=STATUSES, default=STATUS_FORMING, max_length=3)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	status = models.CharField(choices=STATUSES, default=STATUS_FORMING, max_length=3)
+	is_active = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-    def get_total_quantity(self):
-        _items = self.orderitems.select_related()
-        _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _total_quantity
+	@cached_property
+	def get_items_cached(self):
+		return self.orderitems.select_related()
 
-    def get_total_cost(self):
-        _items = self.orderitems.select_related()
-        _total_quantity = sum(list(map(lambda x: x.quantity * x.product.price, _items)))
-        return _total_quantity
+	def get_total_quantity(self):
+		_items = self.get_items_cached
+		_total_quantity = sum(item.quantity for item in _items)
+		return _total_quantity
 
-    def delete(self, using=None, keep_parents=False):
-        self.is_active = False
-        self.save()
+	def get_total_cost(self):
+		_items = self.get_items_cached
+		_total_cost = sum(item.quantity * item.product.price for item in _items)
+		return _total_cost
+
+	def delete(self, using=None, keep_parents=False):
+		self.is_active = False
+		self.save()
+
+	def get_summary(self):
+		items = self.get_items_cached
+		return {
+			'total_cost': sum(list(map(lambda x: x.quantity * x.product.price, items))),
+			'total_quantity': sum(list(map(lambda x: x.quantity, items)))
+		}
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ', related_name='orderitems')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
-    quantity = models.SmallIntegerField(default=0, verbose_name='Количество')
+	order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ', related_name='orderitems')
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+	quantity = models.SmallIntegerField(default=0, verbose_name='Количество')
 
-    def get_product_cost(self):
-        return self.product.price * self.quantity
+	def get_product_cost(self):
+		return self.product.price * self.quantity
--- products/views.py
+++ products/views.py
@@ -2,52 +2,66 @@ from django.shortcuts import render
 from products.models import Product, ProductCategory
 from django.views.generic.list import ListView
 
-from baskets.models import Basket
+from django.core.cache import cache
+from django.conf import settings
+
+
+def get_categories():
+	if settings.LOW_CACHE:
+		key = 'categories'
+		categories = cache.get(key)
+		if categories is None:
+			categories = ProductCategory.objects.all()
+			cache.set(key, categories)
+		return categories
+	else:
+		return ProductCategory.objects.all()
 
 
 def index(request):
-    context = {'title': 'Geekshop'}
-    return render(request, 'products/index.html', context)
+	context = {'title': 'Geekshop'}
+	return render(request, 'products/index.html', context)
 
 
 class ProductsListView(ListView):
-    model = Product
-    template_name = 'products/products.html'
-    paginate_by = 3
+	model = Product
+	template_name = 'products/products.html'
+	paginate_by = 3
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        category_id = self.kwargs.get('pk')
-        if object_list:
-            queryset = object_list
-        else:
-            if category_id:
-                queryset = self.object_list.filter(category_id=category_id)
-            else:
-                queryset = self.object_list
+	def get_context_data(self, *, object_list=None, **kwargs):
+		category_id = self.kwargs.get('pk')
+		if object_list:
+			queryset = object_list
+		else:
+			if category_id:
+				queryset = self.object_list.filter(category_id=category_id)
+			else:
+				queryset = self.object_list
 
-        page_size = self.get_paginate_by(queryset)
-        context_object_name = self.get_context_object_name(queryset)
-        if page_size:
-            paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, page_size)
-            context = {
-                'paginator': paginator,
-                'page_obj': page,
-                'is_paginated': is_paginated,
-                'object_list': queryset
-            }
-        else:
-            context = {
-                'paginator': None,
-                'page_obj': None,
-                'is_paginated': False,
-                'object_list': queryset
-            }
-        if context_object_name is not None:
-            context[context_object_name] = queryset
-        context.update(kwargs)
+		page_size = self.get_paginate_by(queryset)
+		context_object_name = self.get_context_object_name(queryset)
+		if page_size:
+			paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, page_size)
+			context = {
+				'paginator': paginator,
+				'page_obj': page,
+				'is_paginated': is_paginated,
+				'object_list': queryset
+			}
+		else:
+			context = {
+				'paginator': None,
+				'page_obj': None,
+				'is_paginated': False,
+				'object_list': queryset
+			}
+		if context_object_name is not None:
+			context[context_object_name] = queryset
+		context.update(kwargs)
 
-        if self.request.user.is_authenticated:
-            context['basket_content'] = [basket.product.id for basket in self.request.user.basket_set.prefetch_related('product')]
-        context['categories'] = ProductCategory.objects.all()
-        context['title'] = 'GeekShop - Каталог'
-        return context
+		if self.request.user.is_authenticated:
+			context['basket_content'] = [basket.product.id for basket in
+										 self.request.user.basket_set.prefetch_related('product')]
+		context['categories'] = get_categories()
+		context['title'] = 'GeekShop - Каталог'
+		return context
